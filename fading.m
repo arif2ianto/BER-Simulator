25ca5c978ae3a78f7de2791981dd8592ca97f08b
arif2ianto <arifrahmadianarifianto@gmail.com>
2020-12-13 00:53:51 +0700

    Create fading.m

---
+++ fading.m
@@ -0,0 +1,19 @@
+function y = fading(len, fd, T)
+N = 34;
+N0 = (N/2 - 1)/2;
+alpha = pi/4;
+xc = zeros(len,1);
+xs = zeros(len,1);
+sc = sqrt(2)*cos(alpha);
+ss = sqrt(2)*sin(alpha);
+ts = 0:len-1;
+ts = ts'.*T + round(rand(1,1)*10000)*T;
+wd = 2*pi*fd;
+xc = sc.*cos(wd.*ts);
+xs = ss.*cos(wd.*ts);
+for lx =1:N0
+   wn = wd*cos(2*pi*lx/N);
+   xc = xc + (2*cos(pi*lx/N0)).*cos(wn.*ts);
+   xs = xs + (2*sin(pi*lx/N0)).*cos(wn.*ts);
+end;
+y = (xc + i.*xs)./sqrt(N0+1);
